855fec68e111cdb074bf4f422503507ae5ef77f7
savagetushar <pandeytushar287@gmail.com>
2024-06-15 02:54:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const os=require('os');
+console.log("cpu architecture:"+os.arch());
+
+console.log("Free memory:"+os.freemem());
+
+console.log("Total memory:"+os.totalmem());
+
+console.log("Network interfaces:"+os.networkInterfaces());
+
+console.log("os defaulttemp+dir:"+os.tmpdir());
+
+console.log(os.freemem());
+
+console.log(os.cpus());
+
+console.log(os.type());
+
+console.log(os.uptime());
+
+console.log(os.hostname());
+
+
+
+
+
+
